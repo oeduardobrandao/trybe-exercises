4942d924955d4efc7766390ad101a682c59ea297
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-01-24 19:23:09 -0300

    Finalizando exercícios

--- fundamentos/secao3/dia02/script.js
+++ fundamentos/secao3/dia02/script.js
@@ -75,44 +75,120 @@
 
 // Iremos utilizar esse array para realizar os próximos exercícios.
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (let numeros of numbers) {
-//     console.log(numeros)
-// }
-
+    //     console.log(numeros)
+    // }
+    
 // let soma = 0;
 
 // for (i = 0; i < numbers.length; i += 1) {
-//     soma = soma + numbers[i];
-// }
-
-// let media = soma / numbers.length
-
-// if (media > 20) {
-//     console.log('Valor maior que 20')
-// } else {
-//     console.log('Valor menor ou igual a 20')
-// }
-
-// for (let i = 0; i < numbers.length; i+= 1) {
-//     if (numbers[i] >= )
-// }
-
-// numbers
-
+    //     soma = soma + numbers[i];
+    // }
+    
+    // let media = soma / numbers.length
+    
+    // if (media > 20) {
+        //     console.log('Valor maior que 20')
+        // } else {
+            //     console.log('Valor menor ou igual a 20')
+            // }
+            
+            // for (let i = 0; i < numbers.length; i+= 1) {
+                //     if (numbers[i] >= )
+                // }
+                
+                // numbers
+                
 //1. Jogada
-let primeiraJogada = [12, 34, 6, 24, 28, 50];
-let megaSena = [48, 14, 15, 28, 34, 8];
+// let primeiraJogada = [12, 34, 6, 24, 28, 50];
 
 //2. Criando o sorteio
-let primeiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
-let segunddoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
-let terceiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
-let quartoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
-let quintoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
-let sextoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
 
-let megaSenaNumeros = [primeiroNumeroSorteado, segunddoNumeroSorteado, terceiroNumeroSorteado, quartoNumeroSorteado, quintoNumeroSorteado, sextoNumeroSorteado];
+// Criação de números aleatórios
+// let primeiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+// let segunddoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+// let terceiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+// let quartoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+// let quintoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+// let sextoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
 
-console.log(megaSenaNumeros)
+// let megaSenaNumeros = [primeiroNumeroSorteado, segunddoNumeroSorteado, terceiroNumeroSorteado, quartoNumeroSorteado, quintoNumeroSorteado, sextoNumeroSorteado];
+
+// //3. Percorrendo os números do array da pessoa e da megaSena;
+
+// for (let i = 0; i < primeiraJogada.length; i += 1) {
+    //     console.log('Número da minha jogada: ' + primeiraJogada[i])
+    // }
+    
+    // for (let iMegaSena = 0; iMegaSena < megaSenaNumeros.length; iMegaSena += 1) {
+        //     console.log('Número da jogada: ' + megaSenaNumeros[iMegaSena])
+        // }
+        
+        //4. Comparando
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let maiorNumero = numbers[0];
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > maiorNumero) {
+//         maiorNumero = numbers[i]
+//     }
+// }
+
+// console.log(maiorNumero)
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > numbers[i]){
+//         console.log(numbers[i])
+//     }
+// }
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”; 
+
+// let impares = [];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 !== 0){
+//         impares.push(numbers[i]);
+//     } 
+// }
+
+// if (impares.length > 0) {
+//     console.log(impares.length)
+// } else {
+//     console.log('Nenhum valor ímpar encontrado')
+// }
+
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o; 
+
+// let menorNumero = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] < menorNumero) {
+//         menorNumero = numbers[i];
+//     }
+// }
+
+// console.log(menorNumero)
+
+
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado; 
+
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+
+// let vinteCinco = [];
+
+// for (let i = 1; i <= 25; i += 1){
+//     vinteCinco.push(i)
+// }
+
+// // console.log(vinteCinco)
+
+// for (let i = 0; i < vinteCinco.length; i += 1) {
+//     console.log(vinteCinco[i], '/', 2, '=', vinteCinco[i] / 2)
+// }
