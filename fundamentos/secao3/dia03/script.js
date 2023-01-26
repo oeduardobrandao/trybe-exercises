e808d1d7a03e76f2ff6c4c13f8bd5f8f071001b2
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-01-26 14:52:51 -0300

    Finalizando exercícios

--- fundamentos/secao3/dia03/script.js
+++ fundamentos/secao3/dia03/script.js
@@ -13,18 +13,166 @@
 //     }
 //   }
 
-let inicio = 20;
-let fim = 277;
-let numerosDaSequencia = [];
+// let inicio = 20;
+// let fim = 277;
+// let numerosDaSequencia = [];
 
-for (let sequencia = inicio; sequencia <= fim; sequencia += 1){
-    if (sequencia % 3 === 0) {
-        numerosDaSequencia.push(sequencia);
-    } 
+// for (let sequencia = inicio; sequencia <= fim; sequencia += 1){
+//     if (sequencia % 3 === 0) {
+//         numerosDaSequencia.push(sequencia);
+//     } 
+// }
+
+// if (numerosDaSequencia.length > 50) {
+//     console.log('Tem mais de 50 números que dividem por 3')
+// } else {
+//     console.log('Sequência muito pequena')
+// }
+
+
+
+// 1. Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+let fatorial = 5;
+
+for (let mult = fatorial - 1; mult > 0; mult -= 1) {
+    fatorial *= mult;
 }
 
-if (numerosDaSequencia.length > 50) {
-    console.log('Tem mais de 50 números que dividem por 3')
-} else {
-    console.log('Sequência muito pequena')
-}
+console.log(fatorial)
+
+// 2. Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente. 
+
+// Reconhecer string
+// Percorrer todos os seus indexes
+// Criar variável para armazenar os indexes de trás para frente
+// Imprimir nova variável
+
+// let word = 'eduardo';
+// let invertedWord = '';
+
+
+// for (let index = 0; index < word.length; index += 1) {
+//     invertedWord += word[word.length - 1 -index]
+// }
+
+// console.log(invertedWord);
+
+
+// 3.  Considere o array de strings abaixo: 
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra. 
+
+// Criar for para percorrer o array
+// Criar for aninhado para percorrer os indexes do array principal
+// Criar variável para armazenar maior palavra
+// Comparar variávei com demais palavras".lenght"
+// Imprimir variável
+
+// let biggestWord = array[0];
+// let smallestWord = array[0];
+
+// for (let index = 0; index < array.length; index += 1) {
+//         if (array[index].length > biggestWord.length) {
+//             biggestWord = array[index];
+//         }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length < smallestWord.length) {
+//         smallestWord = array[index];
+//     }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
+
+// 4. Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50. 
+
+
+// let num1 = 2;
+// let num2 = 50;
+// let majorPrimo = num1;
+
+// for (let primos = num1; primos <= num2; primos += 1) {
+//     let numeroPrimo = true;
+//     for (let divisores = 2; divisores < primos; divisores += 1) {
+//         if (primos % divisores === 0) {
+//             numeroPrimo = false;
+//         }
+//     }
+//     if (numeroPrimo) {
+//         majorPrimo = primos;
+//     }
+// }
+
+// console.log(majorPrimo);
+
+
+// 5. Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo: 
+
+// let n = 5;
+
+// if (n > 1) {
+//     for (let loops = 1; loops <= n; loops += 1){
+//         console.log('*'.repeat(n))
+//     }
+// }
+
+
+// 6. Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo: 
+
+// if (n > 1) {
+//     for (let loops = 1; loops <= n; loops += 1){
+//         console.log('*'.repeat(loops))
+//     }
+// }
+
+
+// 7. Agora inverta o lado do triângulo. Por exemplo: 
+
+// if (n > 1) {
+//     for (let loops = 1; loops <= n; loops += 1){
+//         console.log(' '.repeat(n - loops) + '*'.repeat(loops))
+//     }
+// }
+
+
+// 8. Depois, faça uma pirâmide com n asteriscos de base [RESOLUÇÃO MAIS CURTA]: 
+
+// if (n > 1) {
+//     for (let loops = 1; loops <= n; loops += 2){
+//         console.log('.'.repeat(n - loops) + '*'.repeat(loops) + '.'.repeat(n - loops))
+//     }
+// }
+
+
+// pedra = 1, papel = 2, tesoura = 3
+
+// 
+
+// let possibilidades = ["pedra", "papel", "tesoura"];
+// player1 = Math.floor(Math.random()*3);
+// player2 = Math.floor(Math.random()*3);
+// if ((player1 === 2 || player2 === 2) && (player1 === 0 || player2 === 0)){
+//     if (player1 === 0){
+//         console.log('Player 1 won');
+//     } else {
+//         console.log('Player 2 won');
+//     }
+// } else {
+//     if (player1 > player2){
+//         console.log('Player 1 won');
+//     } else if (player2 > player1){
+//         console.log('Player 2 won');
+//     } else {
+//         console.log('A tie');
+//     }
+// }
+// console.log(player1);
+// console.log(player2);
+// console.log('Player1: '+possibilidades[player1]);
+// console.log('Player2: '+possibilidades[player2]);
