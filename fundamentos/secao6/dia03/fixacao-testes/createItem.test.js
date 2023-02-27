c7a84432fd12ea1601c936253a3f7c1802de47de
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-02-27 16:19:19 -0300

    Exercícios de fixação

---
+++ fundamentos/secao6/dia03/fixacao-testes/createItem.test.js
@@ -0,0 +1,15 @@
+const createItem = require('./createItem');
+describe('a função createItem', () => {
+  it('cria um item válido', () => {
+    expect(createItem('banana', 'kg', 1.99, 20)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 20 })
+  });
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana', 'kg', 1.99)).toHaveProperty('quantity', 0)
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => { createItem() }).toThrow()
+  });
+  it.todo('Lança um erro se o nome do item não é uma string');
+  it.todo('Lança um erro se o preço é negativo');
+  it.todo('Lança um erro se o preço é zero');
+});
