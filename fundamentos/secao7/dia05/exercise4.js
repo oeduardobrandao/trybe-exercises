23fc1fdb97ec1ad186617c5b4415b8a24d0aef99
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-03-08 18:39:12 -0300

    Finalizando seção

---
+++ fundamentos/secao7/dia05/exercise4.js
@@ -0,0 +1,16 @@
+const student1 = {
+  name: `Claudia`,
+  lastName: `Farias`,
+  age: 23,
+}
+
+const student2 = {
+  name: `Vitor`,
+  age: 20,
+}
+
+// escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+const getLastName = (student) => student.lastName ?? 'lastName não preenchido';
+
+console.log(getLastName(student1));
+console.log(getLastName(student2));
