d3f6466b097f4d585460432e8d83791cf664f6e2
Eduardo Brandao <eduardob.fsa@gmail.com>
2023-06-28 09:26:05 -0300

    Exercícios atrasados

---
+++ front-end/secao2/dia02/exercise-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Image from './Image';
+import cat from './cat.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <Image source={ cat } alternativeText="Cute cat staring" />
+    </div>
+  );
+}
+
+export default App;
